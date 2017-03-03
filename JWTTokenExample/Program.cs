4eca955f7dc7ec9993bc59b69a75673da40e03b7
Naveen-Valmikam <naveen.valmikam@gmail.com>
2017-03-03 14:44:55 +1100

    Changed the code to display validated tokens.

--- JWTTokenExample/Program.cs
+++ JWTTokenExample/Program.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IdentityModel;
-using System.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
-using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace JWTTokenExample
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             //Console.WriteLine("Enter a secret key");
             //var key = Console.ReadLine();
-            //var token = CreateJWTTokenUsingSecretKey(key);            
+            //var token = CreateJWTTokenUsingSecretKey(key);
 
-            var token = GenerateJwtUsingX509Certificate();
-
+            var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkU4NUJCODNFMDY5MEREREIwQjNDQjBDRDM5MDk5M0Y3MkRGOEFFOTQiLCJ0eXAiOiJKV1QifQ.eyJkZXZpZCI6IjZFM0ExNDI1LTVFRUMtNDcxNy04NENBLTQ4RDEzMURCQTZFQiIsImN1c3RpZCI6Ik0xMjM0NTYiLCJuYmYiOjE0ODg1MTIwMzMsImV4cCI6MTQ4OTExNjgzMywiaWF0IjoxNDg4NTEyMDMzLCJpc3MiOiJNTVNHIiwiYXVkIjoiUGF5d2l0aCJ9.TfXfroXdNaE_Au4K9sIS644PX_vRuQrSFRVcEWSxDgXhf5ebM-2qf6YFQbHEjE7gMohQlmaGebC5eL1BS69MGmMsOcoFWx2c5gdJZDgmmdnKfY_rMukyBqxDrIU6LYuSK9Fa3DekXge86pmGwYa6wjIu8qTz3H0fjJ_KZe01eRRdh6ji92vTSQhh4eo6mKCzreJY3c6Jtrn6N0iI-rbZIYr6YmipygelFKVkezn4tqH1svn4Tj48KrI72GrnzZmNQTn-1PvWXX4Brb29C9PCvwcYS3f_CX5VuivlXW0p5nsBQGlG_G52xCMC5RT19nMBhlYGdp76f7-hAfXtUonizg";
+            //var token = GenerateJwtUsingX509Certificate();
             Console.WriteLine("JWT Token : " + token);
             var isTokenValid = ValidateToken(token);
             if(isTokenValid)
@@ -46,19 +41,20 @@ namespace JWTTokenExample
 
             var subject = new ClaimsIdentity(new Claim[]
             {
-                new Claim("Deviceid","6E3A1425-5EEC-4717-84CA-48D131DBA6EB"),
-                new Claim("CustomerId","M123456")
+                new Claim("devid","6E3A1425-5EEC-4717-84CA-48D131DBA6EB"),
+                new Claim("custid","M123456")
             });
 
             tokenDescriptor.Issuer = "MMSG";
             tokenDescriptor.Audience = "Paywith";
+         
             var now = DateTime.UtcNow;
             tokenDescriptor.IssuedAt = now;
-            tokenDescriptor.Expires = now.AddMinutes(2);
+            tokenDescriptor.Expires = now.AddDays(7);
             tokenDescriptor.Subject = subject;
             tokenDescriptor.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256);        
                 
-            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();            
             var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
@@ -105,6 +101,12 @@ namespace JWTTokenExample
                 validationParameters.IssuerSigningKey = new X509SecurityKey(certificate);
                 var validatedToken = tokenHandler.ReadToken(token);
                 var claims = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+                Console.WriteLine(Environment.NewLine);
+                foreach (var claim in claims.Claims)
+                {
+                    Console.WriteLine("Claim Type: " + claim.Type + "\tClaim Value: " + claim.Value);
+                }
+
                 isValidToken = true;
             }
             catch (Exception ex)
@@ -117,13 +119,15 @@ namespace JWTTokenExample
 
         private static X509Certificate2 GetCertificate()
         {
-            var certThumbprint = "B219048772135ECAA97AD1700C090853AD803148";
+            var certThumbprint = "E85BB83E0690DDDB0B3CB0CD390993F72DF8AE94";
+            //var issuerName = "MAXWS2328.msa.com.au";
             var certStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
             certStore.Open(OpenFlags.ReadOnly);
             var certificates = certStore.Certificates.Find(X509FindType.FindByThumbprint, certThumbprint, true);
             if (certificates != null && certificates.Count > 0)
                 return certificates[0];
-
+            //if (certStore != null && certStore.Certificates != null && certStore.Certificates.Count > 0)
+            //return certStore.Certificates[0];
             return new X509Certificate2();
         }
 
@@ -131,7 +135,7 @@ namespace JWTTokenExample
         {
             byte[] bytes = new byte[str.Length * sizeof(char)];
             System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-            return bytes;
+            return Encoding.UTF8.GetBytes(str);
         }
     }
 }
